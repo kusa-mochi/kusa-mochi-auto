59379399e68682c90190e1c1f71aa7e711835190
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-28 17:35:19 +0900

    empty window.

---
+++ KusaMochiAuto/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace KusaMochiAuto.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
