959a87ec0b9b8676740556ee4a7210c1c82e9b54
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-29 16:31:45 +0900

    make a project to practice.

---
+++ Practice/NativeMethods/NativeMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Practice.NativeMethods
+{
+    public static class NativeMethods
+    {
+        [DllImport("User32.dll")]
+        public static extern bool SetCursorPos(int X, int Y);
+    }
+}
---
+++ Practice/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+using Practice.NativeMethods;
+
+namespace Practice.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private DelegateCommand _MouseMoveTo200200;
+        public DelegateCommand MouseMoveTo200200 =>
+            _MouseMoveTo200200 ?? (_MouseMoveTo200200 = new DelegateCommand(ExecuteMouseMoveTo200200));
+
+        void ExecuteMouseMoveTo200200()
+        {
+            NativeMethods.NativeMethods.SetCursorPos(200, 200);
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
