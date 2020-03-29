7b98d1bbf716e2c5f54510570f364113cc6564cc
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-29 16:18:59 +0900

    make base view.

--- KusaMochiAuto/ViewModels/MainWindowViewModel.cs
+++ KusaMochiAuto/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,60 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace KusaMochiAuto.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        #region 変更通知プロパティ
+
+        private bool _IsRecording = false;
+        public bool IsRecording
+        {
+            get { return _IsRecording; }
+            set
+            {
+                SetProperty(ref _IsRecording, value);
+                CanRecord = !value;
+                CanStop = value;
+            }
+        }
+
+        private bool _CanRecord = true;
+        public bool CanRecord
+        {
+            get { return _CanRecord; }
+            set { SetProperty(ref _CanRecord, value); }
+        }
+
+        private bool _CanStop = false;
+        public bool CanStop
+        {
+            get { return _CanStop; }
+            set { SetProperty(ref _CanStop, value); }
+        }
+
+        #endregion
+
+        #region コマンド
+
+        private DelegateCommand _RecordCommand;
+        public DelegateCommand RecordCommand =>
+            _RecordCommand ?? (_RecordCommand = new DelegateCommand(
+                () =>
+                {
+                    IsRecording = true;
+                }));
+
+        private DelegateCommand _StopCommand;
+        public DelegateCommand StopCommand =>
+            _StopCommand ?? (_StopCommand = new DelegateCommand(
+                () =>
+                {
+                    IsRecording = false;
+                }));
+
+        #endregion
+
         public MainWindowViewModel()
         {
 
