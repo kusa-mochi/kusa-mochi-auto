015cbfdfb54cc698a7965c1b53781acaa5b725ec
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-29 16:33:44 +0900

    update practice code.

--- Practice/ViewModels/MainWindowViewModel.cs
+++ Practice/ViewModels/MainWindowViewModel.cs
@@ -1,20 +1,15 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 
-using Practice.NativeMethods;
-
 namespace Practice.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private DelegateCommand _MouseMoveTo200200;
         public DelegateCommand MouseMoveTo200200 =>
-            _MouseMoveTo200200 ?? (_MouseMoveTo200200 = new DelegateCommand(ExecuteMouseMoveTo200200));
-
-        void ExecuteMouseMoveTo200200()
-        {
-            NativeMethods.NativeMethods.SetCursorPos(200, 200);
-        }
+            _MouseMoveTo200200 ?? (_MouseMoveTo200200 = new DelegateCommand(() => {
+                NativeMethods.NativeMethods.SetCursorPos(200, 200);
+            }));
 
         public MainWindowViewModel()
         {
