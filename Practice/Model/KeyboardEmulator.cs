c68beb385d685fd3b5afd7d48be09f1eceafb156
kusa-mochi <whoatemyapplepie@gmail.com>
2020-03-30 18:03:37 +0900

    make base of keyboard emulator.

---
+++ Practice/Model/KeyboardEmulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Practice.NativeMethods;
+
+namespace Practice.Model
+{
+    public class KeyboardEmulator
+    {
+        public bool KeyInput()
+        {
+            return true;
+        }
+
+        public bool KeyDown()
+        {
+            return true;
+        }
+
+        public bool KeyUp()
+        {
+            return true;
+        }
+    }
+}
