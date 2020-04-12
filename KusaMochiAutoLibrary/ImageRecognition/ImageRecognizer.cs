969600834aef1f7c036cfe453dcc325678c63058
kusa-mochi <whoatemyapplepie@gmail.com>
2020-04-12 20:47:22 +0900

    make image recognition base.

--- KusaMochiAutoLibrary/ImageRecognition/ImageRecognizer.cs
+++ KusaMochiAutoLibrary/ImageRecognition/ImageRecognizer.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media;
 using DupImageLib;
 
 namespace KusaMochiAutoLibrary.ImageRecognition
@@ -14,45 +15,46 @@ namespace KusaMochiAutoLibrary.ImageRecognition
     {
         #region Public Methods
 
-        public bool IsImageFound(string imageFilePath)
+        public bool IsImageFound(string imageFilePath, double recognitionThreshold = 0.7)
         {
-            int screenWidth = (int)SystemParameters.VirtualScreenWidth;
-            int screenHeight = (int)SystemParameters.VirtualScreenHeight;
+            System.Drawing.Size screenSize = GetScreenResolution();
+            System.Drawing.Size compareSize = new System.Drawing.Size(16, 16);
 
-            Image imageBitmap = Bitmap.FromFile(imageFilePath);
-            int width = imageBitmap.Width;
-            int height = imageBitmap.Height;
-            MemoryStream imageStream = new MemoryStream();
-            imageBitmap.Save(imageStream, ImageFormat.Bmp);
+            Image image = Bitmap.FromFile(imageFilePath);
+            System.Drawing.Size originalImageSize = new System.Drawing.Size(image.Width, image.Height);
+            image = new Bitmap(image, compareSize);
+            MemoryStream sourceImageStream = new MemoryStream();
+            image.Save(sourceImageStream, ImageFormat.Bmp);
+            Bitmap screenImage = new Bitmap(screenSize.Width, screenSize.Height);
 
-            int nHorizontalRaster = screenWidth - width + 1;
-            int nVerticalRaster = screenHeight - height + 1;
+            int nHorizontalRaster = screenSize.Width - originalImageSize.Width + 1;
+            int nVerticalRaster = screenSize.Height - originalImageSize.Height + 1;
+            int dRow = originalImageSize.Height / 16;
+            int dColumn = originalImageSize.Width / 16;
 
-            for (int iRow = 0; iRow < nVerticalRaster; iRow += 3)
+            using (Graphics g = Graphics.FromImage(screenImage))
+            using (MemoryStream targetStream = new MemoryStream())
             {
-                for (int iColumn = 0; iColumn < nHorizontalRaster; iColumn += 3)
+                g.CopyFromScreen(0, 0, 0, 0, screenSize, CopyPixelOperation.SourceCopy);
+                //screenImage.Save("abababa.bmp", ImageFormat.Bmp);
+                for (int iRow = 0; iRow < nVerticalRaster; iRow += dRow)
                 {
-                    // get a partial screen capture as a memory stream.
-                    Graphics g = Graphics.FromImage(
-                        new Bitmap(width, height, PixelFormat.Format32bppArgb)
-                        );
-                    g.CopyFromScreen(iColumn, iRow, 0, 0, new System.Drawing.Size(width, height), CopyPixelOperation.SourceCopy);
-                    Bitmap bitmap = new Bitmap(width, height, g);
-                    MemoryStream targetImageStream = new MemoryStream();
-                    bitmap.Save(targetImageStream, ImageFormat.Bmp);
+                    for (int iColumn = 0; iColumn < nHorizontalRaster; iColumn += dColumn)
+                    {
+                        Bitmap targetImage = screenImage.Clone(
+                            new Rectangle(iColumn, iRow, originalImageSize.Width, originalImageSize.Height),
+                            System.Drawing.Imaging.PixelFormat.Format32bppArgb
+                            );
+                        targetImage = new Bitmap(targetImage, compareSize);
+                        targetImage.Save(targetStream, ImageFormat.Bmp);
 
-                    // get a value of difference between image streams.
-                    float diff = CompareImages(imageStream, targetImageStream);
-                    if (diff > 0.7) return true;
+                        float diff = CompareImages(sourceImageStream, targetStream);
+                        if (diff > recognitionThreshold) return true;
+                    }
                 }
             }
 
             return false;
-
-            //Rectangle rect = new Rectangle(0, 0, width, height);
-            //Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-            //Graphics g = Graphics.FromImage(bmp);
-            //g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
         }
 
         #endregion
@@ -63,10 +65,25 @@ namespace KusaMochiAutoLibrary.ImageRecognition
         {
             image1.Position = 0L;
             image2.Position = 0L;
-            //ulong[] hash1 = _imageHashes.CalculateDifferenceHash256(image1);
+            ulong hash1 = _imageHashes.CalculateDifferenceHash64(image1);
             ulong hash2 = _imageHashes.CalculateDifferenceHash64(image2);
-            //return ImageHashes.CompareHashes(hash1, hash2);
-            return 0.0f;
+            return ImageHashes.CompareHashes(hash1, hash2);
+        }
+
+        private System.Drawing.Size GetScreenResolution()
+        {
+            Window MainWindow = System.Windows.Application.Current.MainWindow;
+            PresentationSource MainWindowPresentationSource = PresentationSource.FromVisual(MainWindow);
+            Matrix m = MainWindowPresentationSource.CompositionTarget.TransformToDevice;
+            double dpiWidthFactor = m.M11;
+            double dpiHeightFactor = m.M22;
+            double ScreenHeight = SystemParameters.PrimaryScreenHeight * dpiHeightFactor;
+            double ScreenWidth = SystemParameters.PrimaryScreenWidth * dpiWidthFactor;
+
+            return new System.Drawing.Size(
+                (int)ScreenWidth,
+                (int)ScreenHeight
+                );
         }
 
         #endregion
